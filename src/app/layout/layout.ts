c00c29a8964798c476b68eb8dd47de7358a14296
Nikita Jaiswal <nikkijaiswal535@gmail.com>
2025-12-14 14:13:23 +0530

    Responsive Changed

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Storage } from '../core/storage';
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-layout',
@@ -9,6 +9,22 @@ import { Storage } from '../core/storage';
 })
 export class Layout {
 
+  @ViewChild('sidenav') sidenav!: MatSidenav;
 
+  isMobile = false;
 
+  constructor() {
+    this.checkScreen();
+  }
+
+  @HostListener('window:resize')
+  checkScreen() {
+    this.isMobile = window.innerWidth <= 768;
+  }
+
+  onNavClick() {
+    if (this.isMobile) {
+      this.sidenav.close();
+    }
+  }
 }
